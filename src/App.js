b59893abcb930e2dcf619401be68ad0968dd1772
mianzainch000 <mianzainch000@gmail.com>
2023-09-16 12:32:38 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment } from "react";
+import { AutContext } from "./Component/Context";
+import { CompA } from "./Component/CompA";
+import { CompB } from "./Component/CompB";
+import { CompC } from "./Component/CompC";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <AutContext>
+          <CompA />
+          <CompB />
+          <CompC />
+        </AutContext>
+      </div>
+    </Fragment>
   );
 }
 
